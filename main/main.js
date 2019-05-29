8ad1353774cc1b2a10114c6f74b6bdcb996393b9
shenyuxuanok@outlook.com <shenyuxuanok@outlook.com>
2019-05-29 15:55:51 +0800

    Homework of shenyuxuan

--- main/main.js
+++ main/main.js
@@ -1,4 +1,21 @@
 module.exports = function main(inputs) {
     // write your code here...
-    return "price";
+    distance = inputs.distance;
+    parkTime = inputs.parkTime;
+
+    var price_park = 0.25 * parkTime;
+    var price = price_park + 6;
+
+    var base = 0;
+    var extra = 0;
+
+    if(distance > 2){
+        base = 0.8 * (distance - 2);
+        if(distance > 8){
+            extra = 0.8 * 0.5 * (distance - 8);
+        }
+        price += base + extra;
+    }
+
+    return parseInt(price.toFixed());
 };
